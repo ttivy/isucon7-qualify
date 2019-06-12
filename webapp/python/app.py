d239d7a01d9541da3b2cdd65020a97c47c96078d
ttivy <25240747+ttivy@users.noreply.github.com>
2019-06-12 13:53:44 +0900

    アイコンの出力先をDBからファイルに変更

--- webapp/python/app.py
+++ webapp/python/app.py
@@ -366,7 +366,8 @@ def post_profile():
                 avatar_data = data
 
     if avatar_name and avatar_data:
-        cur.execute("INSERT INTO image (name, data) VALUES (%s, _binary %s)", (avatar_name, avatar_data))
+        with open(icons_folder / avatar_name, 'wb') as f:
+            f.wite(avatar_data)
         cur.execute("UPDATE user SET avatar_icon = %s WHERE id = %s", (avatar_name, user_id))
 
     if display_name:
